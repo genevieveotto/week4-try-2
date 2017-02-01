6828ba22ddbfd868f9fdf222de79c356904e03d8
Genevieve Otto <genevieve.otto@kellogg.northwestern.edu>
2017-01-31 20:42:33 -0600

    try again
    try again

---
+++ todo/todo.js
@@ -0,0 +1,11 @@
+$(function() {
+
+  $(".thumbnail").on("click", function(event) {
+    event.preventDefault()
+    var elementThatWasClicked = $(this)
+    console.log(elementThatWasClicked)
+    elementThatWasClicked.parent().remove()
+    // remove the element
+  })
+
+})
